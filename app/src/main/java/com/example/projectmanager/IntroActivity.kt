2b172544e30da37675fbc044aa7721c73a0344e3
Nitesh Kumar <niteshk@geekyants.com>
2022-12-10 19:58:31 +0530

    feat: Added SplashActivity

--- app/src/main/java/com/example/projectmanager/IntroActivity.kt
+++ app/src/main/java/com/example/projectmanager/IntroActivity.kt
@@ -1,11 +1,23 @@
 package com.example.projectmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        findViewById<Button>(R.id.btn_sign_up_intro).setOnClickListener {
+            startActivity(Intent(this@IntroActivity, SignUpActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/projectmanager/SignUpActivity.kt
@@ -0,0 +1,35 @@
+package com.example.projectmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        // This is used to hide the status bar and make the splash screen as a full screen activity.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        /// this is how we add a toolbar to behave like a toolbar
+        var toolbar_sign_up_activity = findViewById<Toolbar>(R.id.toolbar_sign_up_activity)
+        setSupportActionBar(toolbar_sign_up_activity)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+
+        toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
+    }
+}
